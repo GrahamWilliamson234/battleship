20e2e93e6b840e5fc1e3d2de041fa2735c1fb88a
GrahamWilliamson234 <graham@dazl.ie>
2023-01-22 21:07:02 +0000

    def timer countdown added

--- run.py
+++ run.py
@@ -35,3 +35,26 @@ def hello():
     print("Contacting your ships now...  ")
     return
 hello()
+
+def timer():
+    start_timer = time()
+    struct = localtime( start_timer)
+    print("\nContacting Ships At:" , strftime("%x" , struct))
+    i = 10
+    while i > -1:
+        print(i)
+        i -= 1
+        sleep(1)
+    end_timer = time()
+    difference = round(end_timer - start_timer)
+    print("\033[1;31;47m\n")
+    print("")
+    print("       _____  ____                                  ____      ")
+    print("     |   |   |             \            /   /\     |    \     ")
+    print("     |   |   |___           \    /\    /   /  \    | ___/     ")
+    print("     |   |       |           \  /  \  /   /----\   |    \     ")
+    print("     |   |   ____|            \/    \/   /      \  |     \    ")
+    print("                                                              ")
+    return
+timer()
+
