7833b039603bf56398c7593b4eee0d1ab2fcddc0
GrahamWilliamson234 <graham@dazl.ie>
2023-01-22 21:05:12 +0000

    def check_ok added

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randrange
+import random
+from time import *
+
+def check_ok(vessel,taken):
+    
+    vessel.sort()
+    for i in range(len(vessel)):
+        num = vessel[i]
+        if num in taken:
+            vessel = [-1]
+            break            
+        elif num < 0 or num > 99:
+            boat = [-1]
+            break
+        elif num % 10 == 9 and i < len(vessel)-1:
+            if vessel[i+1] % 10 == 0:
+                vessel = [-1]
+                break 
+        if i != 0:
+            if vessel[i] != vessel[i-1]+1 and vessel[i] != vessel[i-1]+10:
+                vessel = [-1]
+                break
+
+    return vessel
