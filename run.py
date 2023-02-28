605764d66d1b4a819652bee9dc4e4ee21d4e1753
GrahamWilliamson234 <graham@dazl.ie>
2023-02-28 17:00:45 +0000

    check setion updated

--- run.py
+++ run.py
@@ -1,22 +1,24 @@
 from random import randrange
 import random
 from time import *
+from time import time, localtime, strftime, sleep
 
-def check_ok(vessel,taken):
-    
+# check
+
+def check_ok(vessel, taken):
     vessel.sort()
     for i in range(len(vessel)):
         num = vessel[i]
         if num in taken:
             vessel = [-1]
-            break            
+            break
         elif num < 0 or num > 99:
             boat = [-1]
             break
         elif num % 10 == 9 and i < len(vessel)-1:
             if vessel[i+1] % 10 == 0:
                 vessel = [-1]
-                break 
+                break
         if i != 0:
             if vessel[i] != vessel[i-1]+1 and vessel[i] != vessel[i-1]+10:
                 vessel = [-1]
